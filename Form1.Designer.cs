076f2dd9ae4c0e026a077fb94e92b74b002f9ff2
Vincent <u26103614@tuks.co.za>
2026-08-29 15:19:57 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace youtube_prac2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbxTitle = new System.Windows.Forms.ComboBox();
+            this.txtInitials = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.nudAge = new System.Windows.Forms.NumericUpDown();
+            this.btnShowSummary = new System.Windows.Forms.Button();
+            this.btnFullDetails = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(371, 30);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(402, 280);
+            this.rtbOutput.TabIndex = 0;
+            this.rtbOutput.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(80, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Title:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(80, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Initials:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(80, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(80, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Surname:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(81, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Age:";
+            // 
+            // cbxTitle
+            // 
+            this.cbxTitle.FormattingEnabled = true;
+            this.cbxTitle.Location = new System.Drawing.Point(164, 96);
+            this.cbxTitle.Name = "cbxTitle";
+            this.cbxTitle.Size = new System.Drawing.Size(115, 21);
+            this.cbxTitle.TabIndex = 7;
+            // 
+            // txtInitials
+            // 
+            this.txtInitials.Location = new System.Drawing.Point(164, 130);
+            this.txtInitials.Name = "txtInitials";
+            this.txtInitials.Size = new System.Drawing.Size(115, 20);
+            this.txtInitials.TabIndex = 8;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(164, 169);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(115, 20);
+            this.txtName.TabIndex = 9;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(164, 215);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(115, 20);
+            this.txtSurname.TabIndex = 10;
+            // 
+            // nudAge
+            // 
+            this.nudAge.Location = new System.Drawing.Point(164, 269);
+            this.nudAge.Name = "nudAge";
+            this.nudAge.Size = new System.Drawing.Size(53, 20);
+            this.nudAge.TabIndex = 11;
+            // 
+            // btnShowSummary
+            // 
+            this.btnShowSummary.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowSummary.Location = new System.Drawing.Point(141, 320);
+            this.btnShowSummary.Name = "btnShowSummary";
+            this.btnShowSummary.Size = new System.Drawing.Size(159, 51);
+            this.btnShowSummary.TabIndex = 12;
+            this.btnShowSummary.Text = "Show  Summary";
+            this.btnShowSummary.UseVisualStyleBackColor = true;
+            this.btnShowSummary.Click += new System.EventHandler(this.btnShowSummary_Click);
+            // 
+            // btnFullDetails
+            // 
+            this.btnFullDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFullDetails.Location = new System.Drawing.Point(410, 320);
+            this.btnFullDetails.Name = "btnFullDetails";
+            this.btnFullDetails.Size = new System.Drawing.Size(159, 51);
+            this.btnFullDetails.TabIndex = 13;
+            this.btnFullDetails.Text = "Show Full Details";
+            this.btnFullDetails.UseVisualStyleBackColor = true;
+            this.btnFullDetails.Click += new System.EventHandler(this.btnFullDetails_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFullDetails);
+            this.Controls.Add(this.btnShowSummary);
+            this.Controls.Add(this.nudAge);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtInitials);
+            this.Controls.Add(this.cbxTitle);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbOutput);
+            this.Name = "Form1";
+            this.Text = "Practical 2";
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbxTitle;
+        private System.Windows.Forms.TextBox txtInitials;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.NumericUpDown nudAge;
+        private System.Windows.Forms.Button btnShowSummary;
+        private System.Windows.Forms.Button btnFullDetails;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace youtube_prac2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFullDetails_Click(object sender, EventArgs e)
+        {
+            // Declaring variables and storing user inputs.
+            string title = cbxTitle.Text;
+            string initials = txtInitials.Text;
+            string name = txtName.Text;
+            string surname = txtSurname.Text;
+            decimal age = nudAge.Value;
+
+            // The else-if statement to check if all fields are entered, else continue with execution.
+            if (title == "" || initials == "" || name == "" ||
+               surname == "" || age == 0)
+            {
+                MessageBox.Show("Please enter all fields!");
+            }
+            else
+            {
+                rtbOutput.Text = "Full Details: " + "\n" + "Title: " + title + "\n" + "Initials: " + initials;
+                rtbOutput.Text += "\n" + "Name: " + name + "\n" + "Surname: " + surname + "\n"+ "Age: " + Convert.ToString(age);
+            }
+        }
+
+        private void btnShowSummary_Click(object sender, EventArgs e)
+        {
+            // Allowing the user input again for this event/event handler.
+            string title = cbxTitle.Text;
+            string surname = txtSurname.Text;
+
+            if(title == "" || surname == "")
+            {
+                MessageBox.Show("Enter both your Title and Surname!");
+            }
+            else
+            {
+                MessageBox.Show("Summary Details: \n" + title + " " + surname);
+            }
+        }
+    }
+}
